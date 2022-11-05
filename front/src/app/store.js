85152550da4449e3998fb1298645136ffbfaa76a
Robert <georobi.016@gmail.com>
2022-11-06 01:15:29 +0200

    added slice for componnets
    slice for components

--- front/src/app/store.js
+++ front/src/app/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
+import pagesReducer from "../slices/pagesSlice"
+import compReducer from '../slices/componentSlice'
 
 export const store = configureStore({
     reducer: {
+        pages: pagesReducer,
+        comps: compReducer
     }
 })
