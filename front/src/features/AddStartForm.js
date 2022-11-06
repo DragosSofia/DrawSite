576f17831b1a208d5395650a645583c0faa6944a
Robert <georobi.016@gmail.com>
2022-11-06 10:46:07 +0200

    last commit
    i love u all

--- front/src/features/AddStartForm.js
+++ front/src/features/AddStartForm.js
@@ -4,8 +4,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { addPages } from "../slices/pagesSlice";
 import { Link } from "react-router-dom";
-
-
+import '../components/Button.css';
 
 function AddStartForm() {
   const dispatch = useDispatch();
@@ -27,12 +26,8 @@ function AddStartForm() {
         url: "http://localhost:8080/post",
         data: formData,
         headers: { "Content-Type": "multipart/form-data" },
-      });
-<<<<<<< HEAD
-      console.log(response);
-=======
+      })
       console.log(response)
->>>>>>> da57f09d2b68b2d0b014d1dcc2d33230e32059d4
     } catch (error) {
       console.log(error);
     }
@@ -53,19 +48,19 @@ function AddStartForm() {
       <h2>Add new page</h2>
       <form>
         <h2>Title of the page</h2>
-        <input
+        <input className="input--field"
           type="text"
           value={name}
           placeholder="Name"
           onChange={onNameChanged}
         />
-
-        <input type="file" onChange={onFileChanged} />
-
-        <button type="button" onClick={handleSubmit}>
+        
+        <input type="file" className='label' onChange={onFileChanged} />
+        
+        <button className='btn--outline' type="button" onClick={handleSubmit}>
           Create
         </button>
-        <button type="button">
+        <button className='btn--outline' type="button" >
           <Link to={`chestionar`}>Next</Link>
         </button>
         <div className="message">{message ? <p>{message}</p> : null}</div>
